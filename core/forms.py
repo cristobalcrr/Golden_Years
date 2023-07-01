70ab372e11471c416dc23c5b398cf4b8ffeef54a
patricio <pa.samur@duocuc.cl>
2023-06-30 23:08:40 -0400

    avance

--- core/forms.py
+++ core/forms.py
@@ -4,5 +4,16 @@ from core.models import Usuario
 class RegistroForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ['nombre', 'apellido','contraseña', 'edad', 'numero', 'foto', 'descripcion']
+        fields = ['nombre', 'apellido', 'contraseña', 'edad', 'numero', 'foto', 'descripcion']
+
+
+        widget = {
+            'nombre': forms.TextInput(attrs={'class':'form-control'}),
+            'apellido': forms.TextInput(attrs={'class':'form-control'}),
+            'contraseña': forms.TextInput(attrs={'class':'form-control'}),
+            'edad': forms.NumberInput(attrs={'class':'form-control'}),
+            'numero': forms.TextInput(attrs={'class':'form-control'}),
+            'foto':forms.ImageField(),
+
+        }
 
--- core/models.py
+++ core/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 
 class Usuario(models.Model):
-    id_usuario = models.CharField(primary_key=True, max_length=6, verbose_name='id ususario')
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     contraseña = models.CharField(max_length=50)
     edad = models.PositiveIntegerField()
-    numero = models.CharField(max_length=20)
+    numero = models.CharField(primary_key=True, max_length=9, verbose_name='id ususario')
     foto = models.ImageField(upload_to='fotos/')
     descripcion = models.TextField()
 
